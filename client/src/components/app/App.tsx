a1cda17e37a5d91311569747cbb88fe7382c9b1c
bora <a52447879@gmail.com>
2022-08-10 07:26:35 +0900

    [feat] react-query 추가 및 설정

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,4 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactElement } from 'react';
 
 import { theme } from '../../theme';
@@ -6,12 +8,17 @@ import { Loading } from './Loading';
 import { Navbar } from './Navbar';
 import { Routes } from './Routes';
 
+const queryClient = new QueryClient();
+
 export function App(): ReactElement {
   return (
-    <ChakraProvider theme={theme}>
-      <Navbar />
-      <Loading />
-      <Routes />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Navbar />
+        <Loading />
+        <Routes />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
